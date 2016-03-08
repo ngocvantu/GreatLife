4b16ce79aa48d16ea42bb9bb1207e7700dd34660
tunguyen <tunguyen4078@gmail.com>
2016-03-08 22:01:51 +0700

    ad

---
+++ trunk/MyFrame1/src/com/main/Main.java
@@ -0,0 +1,20 @@
+package com.main;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) { 
+		Main  main = new Main();
+		main.setLocation(100, 100);
+		main.setSize(100, 200);
+		main.setVisible(true);
+
+	}
+
+}
